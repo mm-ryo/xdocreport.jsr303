abae7a371338daf9c95d6bf3156e50a6ff692071
angelo.zerr@gmail.com <angelo.zerr@gmail.com>
2011-10-28 18:24:59 +0200

    License Header

--- org.eclipse.core.databinding.validation.jsr303.samples.rcp/src/org/eclipse/core/databinding/validation/jsr303/samples/rcp/Activator.java
+++ org.eclipse.core.databinding.validation.jsr303.samples.rcp/src/org/eclipse/core/databinding/validation/jsr303/samples/rcp/Activator.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Angelo Zerr and Pascal Leclercq.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:      
+ *     Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
+ *     Pascal Leclercq <pascal.leclercq@gmail.com> - Initial API and implementation 
+ *******************************************************************************/
 package org.eclipse.core.databinding.validation.jsr303.samples.rcp;
 
 import org.eclipse.jface.resource.ImageDescriptor;
